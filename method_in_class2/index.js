20fdc83cf876d55f4b28173c7973074894bacd07
Dmitriy151 <nova151kra@gmail.com>
2023-11-02 13:05:10 +0600

    02.11

--- method_in_class2/index.js
+++ method_in_class2/index.js
@@ -1,51 +1,8 @@
-// let methodName='calculateAverage';
 
-// class Sportsman{
-//   constructor(){
-        
-//     this.scores=[]; 
-//   }
-  
-//   addScore(score){
-//     this.scores.push(score);
-//   }
-
-//   [methodName](){
-//     if(this.scores.length===0){
-//       return 0;
-//     }
-
-//     let summ =0;
-//     for (let i =0;  i<this.scores.length; i++) {
-//       summ+=this.scores[i];
-//     }
-//     return summ/this.scores.length; 
-
-//   }
-
-// };
-
-// let olga=new Sportsman();
-// olga.addScore(9);
-// olga.addScore(10);
-// olga.addScore(10);
-// olga.calculateAverage();
- 
-
-<<<<<<< HEAD
-let user={
-  name:'Ivan',
-  set birthYear(year){
-    const currentYear=new Date().getFullYear();
-    this.age=currentYear-year;
-  }
-}
-=======
-// let user={
-//   name:'Ivan',
-//   set birthYear(year){
-//     const currentYear=new Date().getFullYear();
-//     this.age=currentYear-year;
-//   }
-// }
->>>>>>> f2e09dedb3bbe801f9956a301a15b59e15383dec
+let user = {
+    name: 'Ivan',
+    set birthYear(year) {
+      const currentYear = new Date().getFullYear();
+      this.age = currentYear - year;
+    }
+  };
---
+++ method_in_class3/index.js
@@ -0,0 +1,18 @@
+
+
+class Human{
+  constructor(name){
+        
+    this.name=name; 
+  }
+};
+
+
+class Man{
+  constructor(name){
+        
+    this.type='мужской пол';
+  }
+};
+
+let user=new Human('Николай Петрович');
