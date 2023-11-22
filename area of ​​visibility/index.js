164c2fd55ea3c6d6762cbeee0078b8cd754acfc9
Dmitriy151 <nova151kra@gmail.com>
2023-11-22 16:28:36 +0600

    22.11

--- area of ​​visibility/index.js
+++ area of ​​visibility/index.js
@@ -7,3 +7,36 @@ function init(){
 }
 
 init();
+
+function createGenerator(start){
+  return function(){
+    
+    console.log(start++);
+  }
+}
+
+let tick=createGenerator(1);
+
+tick();
+tick();
+tick();
+
+let tick10=createGenerator(10);
+
+tick10();
+tick10();
+tick10();
+
+let tick5=createGenerator(5);
+
+tick5();
+tick5();
+tick5();
+
+// function findX() {
+//   function stillfindX() {
+//     function whereIsX() {
+//       console.log(`x=${x}`);
+// }whereIsX();
+// }stillfindX();
+// }
